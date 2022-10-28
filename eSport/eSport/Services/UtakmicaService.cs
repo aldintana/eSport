23b47c3a2574402a3801ea71708808333d24a68a
aldintana <aldin.tanovic@edu.fit.ba>
2022-10-28 15:09:46 +0200

    override update in UtakmicaService

--- eSport/eSport/Services/UtakmicaService.cs
+++ eSport/eSport/Services/UtakmicaService.cs
@@ -49,5 +49,51 @@ namespace eSport.Services
 
             return _mapper.Map<List<Model.Utakmica>>(list);
         }
+
+        public override Model.Utakmica Update(int id, UtakmicaInsertRequest request)
+        {
+            var entity = _context.Utakmicas.Find(id);
+
+            if (entity == null) 
+                return null;
+
+            if(request.IsZavrsena && !entity.IsZavrsena)
+            {
+                List<Database.Tim> timovi = new List<Database.Tim>();
+                var domacin = _context.Tims.Find(request.DomacinId);
+                var gost = _context.Tims.Find(request.GostId);
+                domacin.BrojDatihGolova += request.BrojGolovaDomacina;
+                domacin.BrojPrimljenihGolova += request.BrojGolovaGosta;
+                gost.BrojDatihGolova += request.BrojGolovaGosta;
+                gost.BrojPrimljenihGolova += request.BrojGolovaDomacina;
+                if(request.BrojGolovaDomacina > request.BrojGolovaGosta)
+                {
+                    domacin.BrojPobjeda++;
+                    domacin.BrojBodova += 3;
+                    gost.BrojPoraza++;
+                }
+                else if(request.BrojGolovaDomacina == request.BrojGolovaGosta)
+                {
+                    domacin.BrojNerijesenih++;
+                    domacin.BrojBodova++;
+                    gost.BrojNerijesenih++;
+                    gost.BrojBodova++;
+                }
+                else
+                {
+                    domacin.BrojPoraza++;
+                    gost.BrojPobjeda++;
+                    gost.BrojBodova += 3;
+                }
+                timovi.AddRange(new List<Database.Tim>() { domacin, gost });
+                _context.UpdateRange(timovi);
+            }
+
+            _mapper.Map(request, entity);
+
+            _context.SaveChanges();
+
+            return _mapper.Map<Model.Utakmica>(entity);
+        }
     }
 }
